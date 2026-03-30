4e1ad65d1eac0a392c140db143a37263b00ddb31
jonysins2211 <Jonysins11220@gmail.com>
2026-03-31 01:11:12 +0530

    Add files via upload

---
+++ Filestream/bot/__init__.py
@@ -0,0 +1,6 @@
+# FileStream bot clients registry
+# The main Bot instance is imported from bot.py; multi_clients and work_loads
+# are populated by initialize_clients() during startup.
+
+multi_clients = {}
+work_loads = {}
